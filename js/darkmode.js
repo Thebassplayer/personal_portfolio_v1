a93bb509b5a2f64d037b96248e85b6e64e32a898
Roy Lopez <windup.dues-0f@icloud.com>
2022-10-13 20:45:54 -0300

    dark-mode javascript ready

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,1 +1,38 @@
-let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+const ls = window.localStorage;
+const setValue = (key, value) => {
+  ls.setItem(key, JSON.stringify(value));
+};
+const getValue = (key) => {
+  return JSON.parse(ls.getItem(key));
+};
+const empty = () => {
+  ls.clear();
+};
+let darkMode = getValue("darkMode");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  setValue("darkMode", true);
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  setValue("darkMode", false);
+};
+
+if (darkMode) {
+  enableDarkMode();
+}
+
+// Event listener
+
+darkModeToggle.addEventListener("click", () => {
+  if (darkMode) {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+  darkMode = getValue("darkMode");
+});
